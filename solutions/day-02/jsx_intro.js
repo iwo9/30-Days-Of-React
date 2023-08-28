5abc7ab475e0171bbb40de2c66382cbc7ca7a4f5
iwo9 <maryam.aamir.ashraf@gmail.com>
2023-08-28 11:53:29 +0500

    Halfway through day 2

---
+++ solutions/day-02/jsx_intro.js
@@ -0,0 +1,9 @@
+//Must be a single root html element for every jsx element
+//Any other elements in that jsx element must be wrapped within that root tag
+const header = (
+    <header>
+        <h1>Beginnings</h1>
+        <h2>Intro to Beginnings</h2>
+        <h2>Continuation of Beginnings</h2>
+    </header>
+)
